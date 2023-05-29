8522dae92b502ff953f17d16d4d1d7c8c79e14dc
Gajendhrababu <67729208+Gajendhrababu@users.noreply.github.com>
2023-05-29 15:40:25 +0530

    Update events_view.php

--- events_view.php
+++ events_view.php
@@ -6,21 +6,3 @@ echo ("I Love You!");
 
 
 
-<!doctype html>
-<html class="no-js" lang="zxx">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>EventCon</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-   
-</head>
-
-<body>
-   
-</body>
-
-</html>
